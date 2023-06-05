d6b2de01b85b1cd04c0a87c19e08631bc4908944
nwhator <nwhator@gmail.com>
2023-06-05 16:57:04 -0700

    Refactor: Implement function to check for a cycle in a singly linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Entry point
+ * @list: head of linked list
+ *
+ * Description - Function to check if a linked list has a cycle
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *tail;
+
+	if (!list || !list->next)
+	{
+		return (0);
+	}
+	head = list;
+	tail = list->next;
+	while (head && tail && tail->next)
+	{
+		if (head == tail)
+		{
+			return (1);
+			/* Cycle found */
+		}
+		head = head->next;
+		tail = tail->next->next;
+	}
+	return (0);
+	/* No cycle found */
+}
