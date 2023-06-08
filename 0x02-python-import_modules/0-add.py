1117bc03f69ef08bcc1b527ef94e1968b1d7ed47
nwhator <nwhator@gmail.com>
2023-06-09 00:25:42 +0100

    Addition of two digits

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+if __name__ == "__main__":
+    """Prints the result of the addition 1 + 2 = 3"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    ans = add(a, b)
+    print("{} + {} = {}".format(a, b, add))
