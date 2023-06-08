1eb521f0d2191897550023b6185264a5018f61de
nwhator <nwhator@gmail.com>
2023-06-08 16:47:02 -0700

    Prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# Author: Promise Nwhator
 if __name__ == "__main__":
     """Prints the result of the addition 1 + 2 = 3"""
     from add_0 import add
 
     a = 1
     b = 2
-    ans = add(a, b)
-    print("{} + {} = {}".format(a, b, add))
+    print("{} + {} = {}".format(a, b, add(a, b)))
