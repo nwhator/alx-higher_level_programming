dff85ce09c11830e264843d19c0ebf77e1d08af6
nwhator <nwhator@gmail.com>
2023-06-08 17:42:05 -0700

    Implemented addition of command-line arguments and improved code readability

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+if __name__ == "__main__":
+    """Prints the result of the addition of all arguments"""
+    import sys
+
+    all = 0
+    for i in range(len(sys.argv) - 1):
+        all += int(sys.argv[i + 1])
+    print("{}".format(all))
