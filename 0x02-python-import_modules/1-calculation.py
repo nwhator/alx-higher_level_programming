2dc7e2ea704b5d59c94bf40fabb4657430f10fbf
nwhator <nwhator@gmail.com>
2023-06-08 17:24:28 -0700

    Implemented mathematical operations and result printing in the script, utilizing functions from the calculator_1 module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,12 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    add_ans = add(a, b)
-    sub_ans = sub(a, b)
-    mul_ans = mul(a, b)
-    div_ans = div(a, b)
-
-    print("{} + {} = {}".format(a, b, add_ans))
-    print("{} + {} = {}".format(a, b, sub_ans))
-    print("{} + {} = {}".format(a, b, mul_ans))
-    print("{} + {} = {}".format(a, b, div_ans))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
