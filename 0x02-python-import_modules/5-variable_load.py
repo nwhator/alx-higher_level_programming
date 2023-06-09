672c521da22b7fdbb9420d7506efd625f2bd617f
nwhator <nwhator@gmail.com>
2023-06-08 17:57:03 -0700

    Print the value of variable from variable_load_5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+if __name__ == "__main__":
+    """Print the value of variable from file variable_load_5.py"""
+    from variable_load_5 import a
+
+    print(a)
