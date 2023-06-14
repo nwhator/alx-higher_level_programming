c345421138839aeba303d8f5afff4ec5176d5335
nwhator <nwhator@gmail.com>
2023-06-14 11:42:03 -0700

    Added the update_dictionary function to replace or add key/value pairs in a given dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Update the value of an existing key
+    """
+    a_dictionary[key] = value
+    return a_dictionary
