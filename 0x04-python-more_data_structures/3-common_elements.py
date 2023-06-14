c691a8ab33a3e16bb078dd8fd065520edc095575
nwhator <nwhator@gmail.com>
2023-06-14 11:24:32 -0700

    The function takes two sets as parameters, set_1 and set_2.

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+
+def common_elements(set_1, set_2):
+    """
+    Create an empty set to store the common elements
+    """
+    return(set_1 & set_2)
