0129e3158eff6f103431c6878aa4793b5ac332a8
nwhator <nwhator@gmail.com>
2023-06-14 11:32:59 -0700

    Added the number_keys function to calculate and return the number of keys in a given dictionary.

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: Promise Nwhator
+
+def number_keys(a_dictionary):
+    """
+    Use the built-in len() function to get the num of keys in the dictionary
+    """
+    return len(a_dictionary)
