8b56b19f8426f78260f33d7cac1d372f3d5a31c0
nwhator <nwhator@gmail.com>
2023-07-11 07:10:02 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3 
- ''' Module: has MyList class 
- ''' 
-  
-  
- class MyList(list): 
-     ''' Represents a MyList - sorted printing for the built-in list class 
-     ''' 
-  
-     def print_sorted(self): 
-         ''' 
-         prints the list. 
-         ''' 
-         print(sorted(self))
+#!/usr/bin/python3
+''' Module: has MyList class
+'''
+
+
+class MyList(list):
+    ''' Represents a MyList - sorted printing for the built-in list class
+    '''
+
+    def print_sorted(self):
+        '''
+        prints the list.
+        '''
+        print(sorted(self))
