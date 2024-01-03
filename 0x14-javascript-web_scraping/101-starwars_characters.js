bff7a7dc3422e06bb18fc9b3aef5569444b61a02
nwhator <nwhator@gmail.com>
2024-01-03 04:34:07 -0800

    Script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,38 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 // Get the Movie ID from the command line argument
 const movieID = process.argv[2];
 
 // URL for the Star Wars API
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-// Make a GET request to the specified API endpoint
-request(apiUrl, (error, response, body) => {
-  if (!error) {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    // Fetch and print each character name in order
-    fetchAndPrintCharacters(characterUrls);
-  } else {
-    console.error(`Error: ${error}`);
+// Make a GET request to the specified API endpoint for movie details
+request.get(apiUrl + movieID, function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
+
+  // Parse the JSON response for movie details
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+
+  // Fetch and print each character name in order
+  fetchAndPrintCharacters(characterUrls);
 });
 
 // Function to fetch and print characters in order
 function fetchAndPrintCharacters (urls) {
   urls.forEach((characterUrl) => {
-    request.get(characterUrl, (err, res, charBody) => {
-      if (!err && res.statusCode === 200) {
-        const characterData = JSON.parse(charBody);
-        console.log(characterData.name);
-      } else {
-        console.error(`Error: ${err}`);
+    request.get(characterUrl, function (error, response, characterBody) {
+      if (error) {
+        console.error(error);
       }
+
+      // Parse the JSON response for each character
+      const characterData = JSON.parse(characterBody);
+      console.log(characterData.name);
     });
   });
 }
