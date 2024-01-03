93a4ad8c47b8924e4b3e8a85cd9e868d4145e6a2
nwhator <nwhator@gmail.com>
2024-01-03 03:41:53 -0800

    Semistandard check for task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,6 +6,6 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 // Write string to file in utf-8 encoding
-fs.writeFile(filePath, content, error  => {
-	if (error) console.log(error);
+fs.writeFile(filePath, content, error => {
+  if (error) console.log(error);
 });
