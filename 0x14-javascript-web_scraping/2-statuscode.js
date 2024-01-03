7e48904d6a07bc9a6a7f4b43f218b06de4d9e784
nwhator <nwhator@gmail.com>
2024-01-03 03:46:30 -0800

    Status code of a GET Request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 // Make a GET request to the specified URL
-request.get(url, (error, response) => {
+request.get(url).on('response', function (response) {
   // Display the status code
   console.log(`code: ${response.statusCode}`);
 });
+
