45b099eca5d79b7df0b125efefa272fac40dc812
nwhator <nwhator@gmail.com>
2024-01-10 02:28:27 -0800

    Update text color;of header to read

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the <header> element
+    var header = document.querySelector('header');
+
+    // Update text color to red
+    header.style.color = '#FF0000';
+});
